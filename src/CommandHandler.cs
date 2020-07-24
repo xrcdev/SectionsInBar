df9c43c88d01067b0becca657d569b3bd0e57847
xrcdev <xrcdev@163.com>
2020-07-24 18:42:41 +0800

    不选中时显示鼠标位置;可以多行选中;

--- src/CommandHandler.cs
+++ src/CommandHandler.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
 using System;
 using System.ComponentModel.Composition;
+using System.Text;
 
 namespace ShowSelectionLength
 {
@@ -16,6 +17,7 @@ namespace ShowSelectionLength
     [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
     public class CommandHandler : IWpfTextViewCreationListener
     {
+        private IWpfTextView _textView;
         private static DTE _dte;
 
         public void TextViewCreated(IWpfTextView textView)
@@ -28,8 +30,11 @@ namespace ShowSelectionLength
                 Assumes.Present(_dte);
             }
 
+            _textView = textView;
             textView.Selection.SelectionChanged += SelectionChanged;
+
             textView.Closed += TextView_Closed;
+            //textView.Caret.PositionChanged += CaretPositionChanged;
         }
 
         private void TextView_Closed(object sender, EventArgs e)
@@ -37,6 +42,20 @@ namespace ShowSelectionLength
             var textView = (IWpfTextView)sender;
             textView.Selection.SelectionChanged -= SelectionChanged;
             textView.Closed -= TextView_Closed;
+            //textView.Caret.PositionChanged -= CaretPositionChanged;
+
+        }
+
+        private void CaretPositionChanged(object sender, CaretPositionChangedEventArgs e)
+        {
+            ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+            {
+                var caretText = (ITextCaret)sender;
+
+                var toShowText = caretText.Position;
+
+
+            }).FileAndForget(nameof(ShowSelectionLength));
         }
 
         private void SelectionChanged(object sender, EventArgs e)
@@ -45,27 +64,54 @@ namespace ShowSelectionLength
             {
                 var selection = (ITextSelection)sender;
 
-                if (selection.IsEmpty)
-                {
-                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                    _dte.StatusBar.Clear();
-
-                    return;
-                }
+                //if (selection.IsEmpty)
+                //{
+                //    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                //    _dte.StatusBar.Clear();
+                //}
+                //else
+                //{
+                StringBuilder sbBuilder = new StringBuilder(80);
 
                 int length = 0;
+                var startLine = selection.Start.Position.GetContainingLine();
+                var endLine = selection.End.Position.GetContainingLine();
 
+                //int caretPositionAfterInput = selection.TextView.Caret.Position.BufferPosition;
                 foreach (SnapshotSpan snapshotSpan in selection.SelectedSpans)
                 {
+                    if (sbBuilder.Length == 0)
+                    {
+                        sbBuilder.Append(
+                            $"行:{startLine.LineNumber + 1} 列:{snapshotSpan.Start.Position - startLine.Start.Position + 1}");
+                    }
                     length += snapshotSpan.Length;
                 }
 
                 if (length > 0)
                 {
-                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-                    _dte.StatusBar.Text = $"Selection {length}";
+                    //if (startLine.LineNumber == endLine.LineNumber)
+                    //{
+                    //    sbBuilder.Append($" sel({length})");
+                    //}
+                    //else
+                    //{
+                    sbBuilder.Append($" 选中:{length} | {endLine.LineNumber - startLine.LineNumber + 1}");
+                    //}
                 }
+                var txt = _dte.StatusBar.Text;
+                if (_dte.StatusBar.Text.Contains("行:"))
+                {
+                    var idx = _dte.StatusBar.Text.IndexOf("行:");
+                    txt = _dte.StatusBar.Text.Substring(0, idx);
+                }
+
+                sbBuilder.Insert(0, txt.PadRight(50, (char)32));
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                _dte.StatusBar.Text = sbBuilder.ToString();
+                sbBuilder.Clear();
+                //}
+
             }).FileAndForget(nameof(ShowSelectionLength));
         }
     }
